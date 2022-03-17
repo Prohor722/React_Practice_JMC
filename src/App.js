53adc15387fe96c1e29a2079bc52945b66b28633
Prohor722 <prohor.b722@gmail.com>
2022-03-17 21:55:11 +0600

    dynamicly add data into components using array

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
 import './App.css';
 
 function App() {
-  
+  const persons = ["Sakib","Bappi","Rubel"];
+  return (
+    <div className="App">
+      {
+        persons.map(person=>
+          <Player name={person}></Player>
+        )
+      }
+    </div>
+  );
+}
+
+// Component Player
+function Player(props){
+  return (
+      <h4>Name: {props.name}</h4>
+    )
 }
 
 export default App;
