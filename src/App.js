281c890173f5b4f988de25b1f60bf614edda9d06
Prohor722 <prohor.b722@gmail.com>
2022-03-17 21:56:57 +0600

    dynamicly add data into components using array object

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import './App.css';
 
 function App() {
   const persons = ["Sakib","Bappi","Rubel"];
+  const casts = [{hero:"rajkumar", heroin:"sabila"},{hero:"amir",heroin:"purnima"},{hero:"hero alom",heroin:"deepika"},{hero:"sakib khan", heroin:"angelina"}];
   return (
     <div className="App">
       {
         persons.map(person=>
           <Player name={person}></Player>
         )
+      }
+      {
+        casts.map(cast=><Film hero={cast.hero} heroin={cast.heroin} />)
       }
     </div>
   );
@@ -20,4 +24,13 @@ function Player(props){
     )
 }
 
+// Component Film 
+function Film(props){
+  return (
+    <>
+      <h1>Hero: {props.hero}</h1>
+      <h3>Heroin: {props.heroin}</h3>
+    </>
+  )
+}
 export default App;
